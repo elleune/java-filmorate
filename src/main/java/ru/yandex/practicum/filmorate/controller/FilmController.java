120284958143fc16cb114e3db088f1981f0a97c9
Svetlana <khokhlachevass@gmail.com>
2024-12-11 23:38:02 +0300

    10

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,100 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+    private final Map<Long, Film> films = new HashMap<>();
+    static final LocalDate DATE_BIRTHDAY_CINEMA = LocalDate.of(1895, 12, 28);
+
+    @PostMapping
+    public Film postFilm(@RequestBody Film film) {
+        long filmId = nextFilmId();
+        film.setId(filmId);
+        if (film.getName() == null || film.getName().isBlank()) {
+            log.error("Ошибка валидации фильма");
+            throw new ValidationException("Название не может быть пустым.");
+        }
+        if (film.getDescription() == null || film.getDescription().isBlank() || film.getDescription().length() > 200) {
+            log.error("Ошибка валидации описания");
+            throw new ValidationException("Максимальная длина описания - 200 символов.");
+        }
+        if (film.getReleaseDate() == null || film.getReleaseDate().isBefore(DATE_BIRTHDAY_CINEMA)) {
+            log.error("Ошибка валидации даты создания");
+            throw new ValidationException("Дата реализации - не раньше 28 декабря 1895 года.");
+        }
+        if (film.getDuration() <= 0) {
+            log.error("Ошибка валидации продолжительности фильма");
+            throw new ValidationException("Продолжительность фильма должна быть положительным числом.");
+        }
+        films.put(filmId, film);
+        log.info("Новый фильм создан");
+        return film;
+    }
+
+    @GetMapping
+    public Collection<Film> getFilms() {
+        return films.values();
+    }
+
+    @PutMapping
+    public Film putFilm(@RequestBody Film filmPut) {
+        Film filmTemp = films.get(filmPut.getId());
+        if (filmPut.getReleaseDate() == null) {
+            filmPut.setReleaseDate(filmTemp.getReleaseDate());
+        }
+        if (filmPut.getName() == null) {
+            filmPut.setName(filmTemp.getName());
+        }
+        if (filmPut.getDescription() == null) {
+            filmPut.setDescription(filmTemp.getDescription());
+        }
+        if (filmPut.getDuration() == null) {
+            filmPut.setDuration(filmTemp.getDuration());
+        }
+        if (filmPut.getDescription().length() > 200) {
+            log.error("Описание > 200");
+            throw new ValidationException("Ошибка валидации описания.");
+        }
+        if (filmPut.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.error("Дата релиза позже 28.12.1895");
+            throw new ValidationException("Ошибка валидации даты создания.");
+        }
+        if (filmPut.getDuration() <= 0) {
+            log.error("Отрицательная длина фильма");
+            throw new ValidationException("Ошибка валидации продолжительности фильма.");
+        }
+        if (!films.containsKey(filmPut.getId())) {
+            throw new ValidationException("Фильма с таким ID нет.");
+        }
+        films.put(filmPut.getId(), filmPut);
+        log.info("Фильм изменен");
+        return filmPut;
+    }
+
+
+    private long nextFilmId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
 }
--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,13 +1,152 @@
 package ru.yandex.practicum.filmorate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.yandex.practicum.filmorate.controller.FilmController;
+import ru.yandex.practicum.filmorate.controller.UserController;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 
-@SpringBootTest
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
 class FilmorateApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private UserController userController;
 
+    @Autowired
+    private FilmController filmController;
+
+    @BeforeEach
+    void setUpUser() {
+        userController = new UserController();
+    }
+
+    @Test
+    void createValidUser() {
+        User user = new User();
+        user.setLogin("validLogin");
+        user.setEmail("email@example.com");
+        user.setName("Valid Name");
+        user.setBirthday(LocalDate.of(2001, 11, 1));
+
+        User createUser = userController.postUser(user);
+
+        assertEquals(user.getLogin(), createUser.getLogin());
+        assertEquals(user.getEmail(), createUser.getEmail());
+        assertEquals(user.getName(), createUser.getName());
+        assertEquals(user.getBirthday(), createUser.getBirthday());
+    }
+
+    @Test
+    void createNotValidNameUser() {
+        User user = new User();
+        user.setLogin("");
+        user.setEmail("email@example.com");
+        user.setName("Valid Name");
+        user.setBirthday(LocalDate.of(2001, 11, 1));
+
+        ValidationException exception = assertThrows(ValidationException.class, () -> userController.postUser(user));
+        assertEquals("Логин не может быть пустым и содержать пробелы.", exception.getMessage());
+    }
+
+    @Test
+    void createNotValidEmailUser() {
+        User user = new User();
+        user.setLogin("validLogin");
+        user.setEmail("invalidemail.com");
+        user.setName("Valid Name");
+        user.setBirthday(LocalDate.of(2001, 11, 1));
+
+        ValidationException exception = assertThrows(ValidationException.class, () -> userController.postUser(user));
+        assertEquals("Электронная почта не может быть пустой и должна содержать знак `@`", exception.getMessage());
+    }
+
+    @Test
+    void createNotBirthday() {
+        User user = new User();
+        user.setLogin("validLogin");
+        user.setEmail("email@example.com");
+        user.setName("Valid Name");
+        user.setBirthday(null);
+
+        ValidationException exception = assertThrows(ValidationException.class, () -> userController.postUser(user));
+        assertEquals("День рождения не может быть в будущем.", exception.getMessage());
+    }
+
+    @BeforeEach
+    void setUpFilm() {
+        filmController = new FilmController();
+    }
+
+    @Test
+    void testValidFilm() {
+        Film film = new Film();
+        film.setName("Valid Film");
+        film.setDescription("This is a valid description.");
+        film.setReleaseDate(LocalDate.of(2022, 3, 12));
+        film.setDuration(120);
+
+        Film createFilm = filmController.postFilm(film);
+
+        assertEquals(film.getName(), createFilm.getName());
+        assertEquals(film.getDescription(), createFilm.getDescription());
+        assertEquals(film.getReleaseDate(), createFilm.getReleaseDate());
+        assertEquals(film.getDuration(), createFilm.getDuration());
+    }
+
+    @Test
+    void createNotNameFilm() {
+        Film film = new Film();
+        film.setName("");
+        film.setDescription("This is a valid description."); //
+        film.setReleaseDate(LocalDate.of(2022, 3, 12));
+        film.setDuration(120);
+
+        ValidationException exception = assertThrows(ValidationException.class, () -> filmController.postFilm(film));
+        assertEquals("Название не может быть пустым.", exception.getMessage());
+    }
+
+    @Test
+    void createNotValidDescriptionFilm() {
+        Film film = new Film();
+        film.setName("Valid Film");
+        film.setDescription("A".repeat(201)); // 201 символ
+        film.setReleaseDate(LocalDate.of(2022, 3, 12));
+        film.setDuration(120);
+
+        ValidationException exception = assertThrows(ValidationException.class, () -> filmController.postFilm(film));
+        assertEquals("Максимальная длина описания - 200 символов.", exception.getMessage());
+    }
+
+
+    @Test
+    void createNotValidDuration() {
+        Film film = new Film();
+        film.setName("Valid Film");
+        film.setDescription("This is a valid description.");
+        film.setReleaseDate(LocalDate.of(2022, 3, 12));
+        film.setDuration(-1);
+
+        ValidationException exception = assertThrows(ValidationException.class, () -> filmController.postFilm(film));
+        assertEquals("Продолжительность фильма должна быть положительным числом.", exception.getMessage());
+    }
+
+    @Test
+    void createNotReleaseFilm() {
+        Film film = new Film();
+        film.setName("Valid Film");
+        film.setDescription("This is a valid description.");
+        film.setReleaseDate(LocalDate.of(1895, 11, 27));
+        film.setDuration(120);
+
+        ValidationException exception = assertThrows(ValidationException.class, () -> filmController.postFilm(film));
+        assertEquals("Дата реализации - не раньше 28 декабря 1895 года.", exception.getMessage());
+    }
 }
