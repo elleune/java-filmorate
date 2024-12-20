890c3191a0f54dd8af72503bdbd68b5dcfc4ebe5
elleune <156022042+elleune@users.noreply.github.com>
2024-12-21 00:00:58 +0300

    Update FilmController.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
@@ -24,41 +22,34 @@ public class FilmController {
     private final FilmService filmService;
 
 
-    @PutMapping("/films/{id}/like/{userId}")
+    @PutMapping("/{id}/like/{userId}")
     public void likeFilm(@PathVariable("id") long filmId, @PathVariable long userId) {
         filmService.likeFilm(filmId, userId);
     }
 
 
-    @DeleteMapping("/films/{id}/like/{userId}")
+    @DeleteMapping("/{id}/like/{userId}")
     public void removeLikeFromFilm(@PathVariable("id") long filmId, @PathVariable long userId) {
         filmService.removeLikeFromFilm(filmId, userId);
     }
 
-
-    @GetMapping("/films/popular")
-    public List<Film> getTopFilms(@RequestParam(defaultValue = "10") @Positive int count) {
-        return filmService.getTopFilms(count);
-    }
-
-
-    @PostMapping("/films")
+    @PostMapping
     public Film create(@Valid @RequestBody Film film) {
         return filmService.createFilm(film);
     }
 
 
-    @PutMapping("/films")
+    @PutMapping
     public Film update(@Valid @RequestBody Film newFilm) {
         return filmService.updateFilm(newFilm);
     }
 
-    @GetMapping("/films")
+    @GetMapping
     public List<Film> findAllFilms() {
         return filmService.findAllFilms();
     }
 
-    @GetMapping("/films/{id}")
+    @GetMapping("/{id}")
     public Film getFilmById(@PathVariable long id) {
         return filmService.getFilmById(id);
     }
