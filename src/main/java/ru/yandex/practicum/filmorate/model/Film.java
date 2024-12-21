a828a0ee83343bab10a1419b32633db81c305aa7
elleune <156022042+elleune@users.noreply.github.com>
2024-12-21 22:00:42 +0300

    Update Film.java

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -23,6 +24,7 @@ public class Film {
     @Size(max = 200, message = "Максимальная длина описания - 200 символов.")
     private String description;
     private LocalDate releaseDate;
-    private Duration duration;
     private Set<Long> idUserLike = new HashSet<>();
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER_INT)
+    private Duration duration;
 }
