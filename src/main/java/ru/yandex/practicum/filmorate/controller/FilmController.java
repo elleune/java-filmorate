82afd0867394a932b638a966430f1dae3ed3702f
elleune <156022042+elleune@users.noreply.github.com>
2024-12-24 22:22:25 +0300

    Update FilmController.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -12,10 +12,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.exception.IncorrectCountException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/films")
@@ -35,7 +37,7 @@ public class FilmController {
     }
 
     @GetMapping("/popular")
-    public List<Film> getPopularFilms(@RequestParam(required = false) @Positive Integer count) {
+    public List<Film> getPopularFilms(@RequestParam(required = false) @Positive Integer count) throws IncorrectCountException {
         return filmService.getPopularFilms(count);
     }
 
