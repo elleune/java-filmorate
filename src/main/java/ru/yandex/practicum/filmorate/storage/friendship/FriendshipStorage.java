5b3495158ccc73040cca5029c5176bf68390096e
Светлана <156022042+elleune@users.noreply.github.com>
2025-03-03 21:26:46 +0300

    Update FriendshipStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/friendship/FriendshipStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/friendship/FriendshipStorage.java
@@ -5,11 +5,11 @@ import ru.yandex.practicum.filmorate.model.Friendship;
 import java.util.List;
 
 public interface FriendshipStorage {
+    List<Friendship> findAll();
 
-    List<Friendship> getAll();
+    void create(long userId, long friendId);
 
-    void create(Long userId, Long friendId);
+    void remove(long userId, long friendId);
 
-    void remove(Long userId, Long friendId);
-
+    void clear();
 }
