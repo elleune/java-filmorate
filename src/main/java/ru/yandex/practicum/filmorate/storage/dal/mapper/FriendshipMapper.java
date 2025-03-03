7944d34c9dcc303d4638fd79f90f9289032e1198
Светлана <156022042+elleune@users.noreply.github.com>
2025-03-03 19:54:48 +0300

    Update FriendshipMapper.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/dal/mapper/FriendshipMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dal/mapper/FriendshipMapper.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.filmorate.storage.dal.mapper;
 
-package ru.yandex.practicum.filmorate.storage.dal.mapper;
-
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Friendship;
