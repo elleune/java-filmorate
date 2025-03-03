6f0019386ff9d0652d15c1996821cfc072fd58e1
Светлана <156022042+elleune@users.noreply.github.com>
2025-03-03 19:23:44 +0300

    Update Friendship.java

--- src/main/java/ru/yandex/practicum/filmorate/model/Friendship.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Friendship.java
@@ -2,13 +2,15 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+@Data
 @AllArgsConstructor
-@EqualsAndHashCode(exclude = "isAccepted")
+@EqualsAndHashCode(exclude = "isConfirmed")
 @Builder
 public class Friendship {
     Long userId;
     Long friendId;
-    boolean isAccepted;
+    boolean isConfirmed;
 }
