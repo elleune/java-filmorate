f3f4e8272386bbcbe7ffe4bef341be3efc4d1bd4
Светлана <156022042+elleune@users.noreply.github.com>
2025-03-03 21:26:29 +0300

    Update FriendshipDbStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/friendship/FriendshipDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/friendship/FriendshipDbStorage.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.storage.friendship;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Friendship;
 import ru.yandex.practicum.filmorate.storage.dal.repository.FriendshipRepository;
@@ -8,24 +9,28 @@ import ru.yandex.practicum.filmorate.storage.dal.repository.FriendshipRepository
 import java.util.List;
 
 @Component
+@Primary
 @RequiredArgsConstructor
 public class FriendshipDbStorage implements FriendshipStorage {
-
     final FriendshipRepository friendshipRepository;
 
     @Override
-    public List<Friendship> getAll() {
+    public List<Friendship> findAll() {
         return friendshipRepository.findAll();
     }
 
     @Override
-    public void create(Long userId, Long friendId) {
+    public void create(long userId, long friendId) {
         friendshipRepository.create(userId, friendId);
     }
 
     @Override
-    public void remove(Long userId, Long friendId) {
+    public void remove(long userId, long friendId) {
         friendshipRepository.remove(userId, friendId);
     }
 
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("Очистка таблицы БД не поддерживается");
+    }
 }
